a8ef0f68eed3fe39ebfeef81424946fcb4be3b0b
babikdonat <114010364+babikdonat@users.noreply.github.com>
2023-05-13 21:04:16 +0200

    Add files via upload

--- HAZI/HAZI11/HAZI11.py
+++ HAZI/HAZI11/HAZI11.py
@@ -30,6 +30,7 @@ return type: keras.engine.sequential.Sequential
 függvény neve: cifar100_model
 '''
 def cifar100_model():
+    model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
@@ -37,6 +38,8 @@ def cifar100_model():
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(100, activation='softmax')
+    ])
+    return model
 
 
 
